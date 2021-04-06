2717231d379cce1e9f7482093d9ae23b14b1eba6
Akseli Keinänen <keinanenakseli@gmail.com>
2021-04-06 19:32:23 +0300

    Muokattu polkuja

--- js/skole.js
+++ js/skole.js
@@ -3,3 +3,12 @@ opetussivusto "Skole"
 Markus Erkkilä, Matleena Autio, Santeri Pirilä, Akseli Keinänen, Vadim Panfilov
 31.3.2021 */
 
+$(window).on('load', () => {
+    console.log(window.location.href)
+    $('.main-nav li a').each(() => {
+        console.log($(this));
+        if($(this).attr('href') === window.location.href) {
+            $(this).addClass('active');
+        }
+    });
+});
